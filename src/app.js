57f3bfdca2e570df393d12b7ae31e1dbd66a3d5b
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2015-09-05 12:10:29 -0500

    ayyy lmao it works

--- src/app.js
+++ src/app.js
@@ -7,62 +7,81 @@ var lTrigger = 100;
 var rTrigger = 100;
 var rawLeft;
 var rawRight;
-var alertStatus = "Loading...";
+var alertStatus = "test...";
+var heartRate = "Loading...";
 
 var URL = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk';
 
+
+
 var window = new UI.Menu({
-  sections: [{
-    items: [{
-      title: 'Digital Coffee',
-      icon: 'images/logoMini.png',
-      subtitle: 'You\'ll stay awake!'
-    }, {
-      title: 'Alert Status:',
-      subtitle: 'Loading...'
-    }, {
-      title: 'Values (L, R)',
-      subtitle: 'Loading...'
-    }]
-  }]
+	sections: [{
+		items: [{
+			title: 'Digital Coffee',
+			icon: 'images/logoMini.png',
+			subtitle: 'You\'ll stay awake!'
+		}, {
+			title: 'Alert Status:',
+			subtitle: 'Loading...'
+		}, {
+			title: 'Values (L, R):',
+			subtitle: 'Loading...'
+		}, {
+			title: 'Heart Rate:',
+			subtitle: 'Loading...'
+		}]
+	}]
 });
 
 window.show();
 
 setInterval(refreshData, 500);
+
 function refreshData(){
-  ajax({url: URL, type: 'json'},
-    function(json){
-      updateData();
-      rawLeft = json.weather[0].main;
-      rawRight = json.weather[0].main;
-      if(rawLeft > lTrigger && rawRight > rTrigger){
-        Vibe.vibrate('short');        
-      }
-    },
-    function(error) {
-      Vibe.vibrate('long');
-    }
-  );
-  
+	ajax({url: URL, type: 'json'},
+		function(json){
+			updateData();
+			rawLeft = json.weather[0].main;
+			rawRight = json.weather[0].main;
+			if(rawLeft > lTrigger && rawRight > rTrigger){
+				Vibe.vibrate('short');
+			}
+		},function(error) {
+				Vibe.vibrate('long');
+		}
+	);
 }
 
 function updateData(left, right, status){
-  var window = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Digital Coffee',
-        icon: 'images/logoMini.png',
-        subtitle: 'You\'ll stay awake!'
-      }, {
-        title: 'Alert Status:',
-        subtitle: alertStatus
-      }, {
-        title: 'Values (L, R)',
-        subtitle: 'L:' + rawLeft + ' R:' + rawRight
-      }]
-    }]
-  });
-
-  window.show();
+	/*window = new UI.Menu({
+		sections: [{
+			items: [{
+				title: 'Digital Coffee',
+				icon: 'images/logoMini.png',
+				subtitle: 'You\'ll stay awake!'
+			}, {
+				title: 'Alert Status:',
+				subtitle: alertStatus
+			}, {
+				title: 'Values (L, R):',
+				subtitle: 'L:' + rawLeft + ' R:' + rawRight
+			}, {
+				title: 'Heart Rate:',
+				subtitle: heartRate
+			}]
+		}]
+	});*/
+	
+	window.item(0, 1, {
+		title: 'Alert Status:',
+		subtitle: alertStatus
+	});
+	window.item(0, 2, {
+		title: 'Values (L, R):',
+		subtitle: 'L:' + rawLeft + ' R:' + rawRight
+	});
+	window.item(0, 3, {
+		title: 'Heart Rate:',
+		subtitle: heartRate
+	});
 }
